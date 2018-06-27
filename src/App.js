fb901187ab65fd79d9d27f367ecbbcd733e0bb36
Candace <candace@Candaces-MacBook-Pro.local>
2018-06-27 15:51:44 -0400

    removed flavorlist, not needed. Updated Search.js and App.js to add filter

--- src/App.js
+++ src/App.js
@@ -1,47 +1,51 @@
 import React, { Component } from 'react';
 import sampleFlavors from './sample-flavors';
 import IceCream from './components/IceCream';
-import FlavorList from './components/FlavorList';
 import Search from './components/Search';
 import './App.css';
 
 class App extends Component {
 
   state = {
-    flavors:{}
+    flavors:{},
+    filterText: ''
   }
 
-//
-//   loadSampleFlavors = () =>{
-//    this.setState({flavors: sampleFlavors}, () => console.log(this.state.flavors));
-//   //alert('hello');
-// };
-// //
 componentDidMount = () => {
   this.setState({flavors: sampleFlavors});
+  this.setState({filterText: ''});
+
 };
 
+filterUpdate(value){
+  this.setState({filterText:value});
+}
+
+
+
     render() {
-
-
+      // console.log('filtertext state', this.state.filterText);
+    const hasSearch =  this.state.filterText ? this.state.filterText.length : 0;
     return (
+
       <div className="App">
-            <Search />
+            <Search
+              filterVal = {this.state.filterText}
+              filterUpdate={this.filterUpdate.bind(this)}
+
+            />
          <ul className="IceCream">
             {Object.keys(this.state.flavors).map(key =>(
               <IceCream
                  key={key}
                  details={this.state.flavors[key]}
+                 filter={this.state.filterText}
+
                />
 
              ))}
-             <FlavorList
-               flavors = {this.state.flavors}/>
+
           </ul>
-
-
-
-
 
 
         </div>
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,38 +1,41 @@
 import React, { Component } from 'react';
 
 
+
 class Search extends Component {
-  nameRef = React.createRef();
-
-  searchFlavor = event => {
-    const val = {
-      name: this.nameRef.current.value
-
-  }
-
-  console.log(val);
-
- };
-
-
+//   nameRef = React.createRef();
+//
+//   searchFlavor = event => {
+//     const val = {
+//       name: this.nameRef.current.value
+//
+//   }
+//
+//   console.log(val);
+//
+// };
   render() {
+    const {filterVal, filterUpdate} = this.props
+    console.log(filterVal);
 
     return (
       <div>
 
-  <form className="Search" onChange ={this.searchFlavor.bind(this)} >
+  <form className="Search">
     <input name="name"
-      ref={this.nameRef}
+      ref='filterInput'
       type="text"
       placeholder="Search Here"
+      value={filterVal}
+      onChange = {() => {
+         filterUpdate(this.refs.filterInput.value)
+      }}
      />
   </form>
 </div>
 
 
-
     )};
-
 
 
 }
